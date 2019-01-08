c9404b3a3accd2d10a4076025e47f8bd995debf4
sourabh <sourabh181097@gmail.com>
2019-01-08 23:05:43 +0530

    files added

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -15,6 +15,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func showAlert(){
+        let alert = UIAlertController(title: "Hello there", message: "This is my first app", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    @IBAction func showKnock(){
+        let alert = UIAlertController(title: "ohh", message: "who is there", preferredStyle: .alert)
+        let action = UIAlertAction(title: "close", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
