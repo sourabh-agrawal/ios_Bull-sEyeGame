9aa184f733f87d0879c30874d03c9f966c2ff42d
sourabh <sourabh181097@gmail.com>
2019-01-09 18:20:44 +0530

    added 4 new files to the list

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -7,15 +7,26 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
-
+    
+    @IBOutlet weak var webView : WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if let htmlPath = Bundle.main.path(forResource: "BullsEye", ofType: "html"){
+            let url = URL(fileURLWithPath: htmlPath)
+            let reuest = URLRequest(url: url)
+            webView.load(reuest)
+        }
     }
+    
+    
     @IBAction func close(){
         dismiss(animated: true, completion: nil)
     }
+    
 }
